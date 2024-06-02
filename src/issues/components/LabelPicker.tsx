72107d3a1115eecb92d776ff6547f9ced8f14eb9
Alvaro Neira <alvaroneira98@gmail.com>
2024-06-01 21:27:12 -0500

    agregando dependencias de react-query y su devtools, peticion de labels issues

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/Label";
+
+
+const getLabels = async () :Promise<Label[]> => {
+  const {data} = await githubApi.get<Label[]>('/labels')
+  console.log(data)
+  return data
+}
+
 
 export const LabelPicker = () => {
+  // Desactivar focus
+  const { data: dataLabels, isLoading, isError } = useQuery({
+    queryKey: ["labels"],
+    queryFn: getLabels,
+    refetchOnWindowFocus() {
+      return false;
+    },
+  });
+
   return (
-    <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
-    </div>
-  )
-}
+    <>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error</p>}
+      {dataLabels &&
+        dataLabels?.map((
+          label: { name: string; color: string; id: number },
+          index: number
+         ) => (
+          <div key={label.id}>
+            <span
+              className="badge rounded-pill m-1 label-picker"
+              style={{ border: `1px solid ${label.color}`, color: label.color }}
+            >
+              {label.name}
+            </span>
+          </div>
+        ))}
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,15 @@ import { router } from './router';
 //! Remover enable css source maps
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <QueryClientProvider client={client}>
+    <ReactQueryDevtools/>
     <RouterProvider router={ router } />
-  </React.StrictMode>
+  </QueryClientProvider>
+  
 )
